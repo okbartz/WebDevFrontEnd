593f8a1c04ff9d8d9564547611b92aaf3746d784
okbartz <bartk1612@gmail.com>
2024-10-08 13:16:33 +0100

    Lab 2 Finished

--- Lab2/js/eventlistners.js
+++ Lab2/js/eventlistners.js
@@ -11,20 +11,55 @@
 //    let mydiv=document.getElementById(divname);
 //    mydiv.style.display = "none";        
 //  }
-const western = document.get
+const western = document.getElementById("westerntitle")
+const borneo = document.getElementById("borneotitle")
 
-western.addEventListener(
-  "mouseover",
-  (event) => {
+function registerHandlers()
+{
     
+    western.addEventListener('mouseover',handleWestern, false);
+    
+    borneo.addEventListener('mouseover',handleBorneo, false);
+    alert("Handlers registered");
+}
+
+function removeHandlers()
+{
+    
+    western.removeEventListener('mouseover',handleWestern, false);
+   
+    borneo.removeEventListener('mouseover',handleBorneo, false);
+    alert("Handlers removed");
+}
+
+
+function handleWestern()
+{
+    expand("western");
+    collapse("borneo");
+}
+
+function handleBorneo()
+{
+    expand("borneo");
+    collapse("western");
+}
+
+
+
+function expand(divname)
+{
     let mydiv=document.getElementById(divname);
-    mydiv.style.display = "block";
+    mydiv.style.display = "block";    
+}
 
+function collapse(divname)
+{
+    let mydiv=document.getElementById(divname);
+    mydiv.style.display = "none";        
+}
 
-
-    setTimeout(() => {
-      event.target.style.color = "";
-    }, 500);
-  },
-  false,
-);
+window.onload = function(){
+  this.document.getElementById("register").addEventListener('click',registerHandlers, false);
+  this.document.getElementById("remove").addEventListener('click',removeHandlers, false);
+}
