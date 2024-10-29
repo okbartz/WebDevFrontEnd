ab75be99444ba2d1842f717c0fcc9ca5bdc3e1a7
okbartz <bartk1612@gmail.com>
2024-10-29 13:44:14 +0000

    Lab 5 finished

--- Lab3/lab3/src/components/People.js
+++ Lab3/lab3/src/components/People.js
@@ -1,7 +1,7 @@
 import React from "react";
-import{staff} from './data/staff';
+import { Link, Outlet } from "react-router-dom";
 
-const People = () => {
+const People = ({staff}) => {
   return (
     <div className="container-fluid">
       <h2>Meet our Staff</h2>
@@ -9,11 +9,13 @@ const People = () => {
       <ul className="list-group">
         {staff.map((person) => (
           <li className="list-group-item" key={person.id}>
-            {person.fullName}
+            <Link to={person.id}>{person.fullName}</Link>
           </li>
         ))}
       </ul>
+      <Outlet />
     </div>
   );
 };
+
 export default People;
